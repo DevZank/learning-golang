e7e14ba36b31e431f9956ad0ca58c9fd05529941
Isaac Martins <voltzinx1@gmail.com>
2026-09-15 18:09:10 -0300

    types

---
+++ types/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("Type: %T - Value: %v\n", true, false)
+	fmt.Printf("Type: %T - Value: %v\n", "eu", "eu")
+	fmt.Printf("Type: %T - Value: %v\n", 1, 1)
+	fmt.Printf("Type: %T - Value: %v\n", "1", "1")
+	fmt.Printf("Type: %T - Value: %v\n", 1.5, 1.5)
+}
